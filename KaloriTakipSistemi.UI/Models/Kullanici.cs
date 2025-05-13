c319aa7c60a290ec78ad71bbaaa6b296482882ca
Doğa Yıldız <dogasuyildizz@gmail.com>
2025-05-13 20:33:17 +0300

    Kullanici classi ve propertyleri olusturur.

---
+++ KaloriTakipSistemi.UI/Models/Kullanici.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KaloriTakipSistemi.UI.Models
+{
+    public class Kullanici
+    {
+        public int Id { get; set; }
+        public string AdSoyad { get; set; }
+        public string Sifre { get; set; }
+        public string KullaniciAdi { get; set; }
+        public byte Yas { get; set; }
+        public bool HesapDurumu { get; set; }
+        public List<KullaniciYemek> KullaniciYemek { get; set; }
+    }
+}
