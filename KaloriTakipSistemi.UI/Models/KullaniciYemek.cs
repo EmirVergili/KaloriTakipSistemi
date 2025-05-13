08690eb042a291f23e89ccd61b9206dd1a7f73aa
Erhan Simsek <erhansimsek47@hotmail.com>
2025-05-13 20:44:10 +0300

    KullaniciYemek Class ekler

---
+++ KaloriTakipSistemi.UI/Models/KullaniciYemek.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KaloriTakipSistemi.UI.Models
+{
+    public class KullaniciYemek
+    {
+        public int KullaniciId { get; set; }
+
+        public Kullanici Kullanici { get; set; }
+
+        public int YemekId { get; set; }
+
+        public Yemek Yemek { get; set; }
+
+        public DateTime YemekTarihi { get; set; }
+        public int Miktar { get; set; }
+        public int OgunId { get; set; }
+        public Ogun Ogun { get; set; }
+
+    }
+}
