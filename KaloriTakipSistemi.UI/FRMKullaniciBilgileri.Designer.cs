c2c38b0a509a609eb4c7ac3eeaa8b0b257bf4f16
berkay <dastanberkay@gmail.com>
2025-05-14 15:34:22 +0300

    Kullanici Bilgileri EKrani Olusturuldu

---
+++ KaloriTakipSistemi.UI/FRMKullaniciBilgileri.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace KaloriTakipSistemi.UI
+{
+    partial class FRMKullaniciBilgileri
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            lbl = new Label();
+            label2 = new Label();
+            lblSoyad = new Label();
+            lblYas = new Label();
+            lblSifre = new Label();
+            txtSoyad = new TextBox();
+            txtYas = new TextBox();
+            txtSifre = new TextBox();
+            txtAd = new TextBox();
+            btnGuncelle = new Button();
+            lblKullaniciAdi = new Label();
+            chbBilgilerimiGuncelle = new CheckBox();
+            SuspendLayout();
+            // 
+            // lbl
+            // 
+            lbl.AutoSize = true;
+            lbl.Location = new Point(26, 43);
+            lbl.Margin = new Padding(4, 0, 4, 0);
+            lbl.Name = "lbl";
+            lbl.Size = new Size(95, 21);
+            lbl.TabIndex = 0;
+            lbl.Text = "Kullanıcı Adı";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(37, 111);
+            label2.Margin = new Padding(4, 0, 4, 0);
+            label2.Name = "label2";
+            label2.Size = new Size(29, 21);
+            label2.TabIndex = 1;
+            label2.Text = "Ad";
+            // 
+            // lblSoyad
+            // 
+            lblSoyad.AutoSize = true;
+            lblSoyad.Location = new Point(37, 183);
+            lblSoyad.Margin = new Padding(4, 0, 4, 0);
+            lblSoyad.Name = "lblSoyad";
+            lblSoyad.Size = new Size(55, 21);
+            lblSoyad.TabIndex = 2;
+            lblSoyad.Text = "SoyAd";
+            // 
+            // lblYas
+            // 
+            lblYas.AutoSize = true;
+            lblYas.Location = new Point(37, 256);
+            lblYas.Margin = new Padding(4, 0, 4, 0);
+            lblYas.Name = "lblYas";
+            lblYas.Size = new Size(33, 21);
+            lblYas.TabIndex = 3;
+            lblYas.Text = "Yaş";
+            // 
+            // lblSifre
+            // 
+            lblSifre.AutoSize = true;
+            lblSifre.Location = new Point(37, 318);
+            lblSifre.Margin = new Padding(4, 0, 4, 0);
+            lblSifre.Name = "lblSifre";
+            lblSifre.Size = new Size(42, 21);
+            lblSifre.TabIndex = 4;
+            lblSifre.Text = "Şifre";
+            // 
+            // txtSoyad
+            // 
+            txtSoyad.Location = new Point(140, 179);
+            txtSoyad.Margin = new Padding(4, 4, 4, 4);
+            txtSoyad.Name = "txtSoyad";
+            txtSoyad.Size = new Size(127, 29);
+            txtSoyad.TabIndex = 6;
+            // 
+            // txtYas
+            // 
+            txtYas.Location = new Point(140, 245);
+            txtYas.Margin = new Padding(4, 4, 4, 4);
+            txtYas.Name = "txtYas";
+            txtYas.Size = new Size(127, 29);
+            txtYas.TabIndex = 7;
+            // 
+            // txtSifre
+            // 
+            txtSifre.Location = new Point(140, 314);
+            txtSifre.Margin = new Padding(4, 4, 4, 4);
+            txtSifre.Name = "txtSifre";
+            txtSifre.Size = new Size(127, 29);
+            txtSifre.TabIndex = 8;
+            // 
+            // txtAd
+            // 
+            txtAd.Location = new Point(140, 106);
+            txtAd.Margin = new Padding(4, 4, 4, 4);
+            txtAd.Name = "txtAd";
+            txtAd.Size = new Size(127, 29);
+            txtAd.TabIndex = 10;
+            // 
+            // btnGuncelle
+            // 
+            btnGuncelle.Location = new Point(231, 451);
+            btnGuncelle.Margin = new Padding(4, 4, 4, 4);
+            btnGuncelle.Name = "btnGuncelle";
+            btnGuncelle.Size = new Size(96, 32);
+            btnGuncelle.TabIndex = 12;
+            btnGuncelle.Text = "Güncelle";
+            btnGuncelle.UseVisualStyleBackColor = true;
+            // 
+            // lblKullaniciAdi
+            // 
+            lblKullaniciAdi.AutoSize = true;
+            lblKullaniciAdi.Location = new Point(172, 43);
+            lblKullaniciAdi.Margin = new Padding(4, 0, 4, 0);
+            lblKullaniciAdi.Name = "lblKullaniciAdi";
+            lblKullaniciAdi.Size = new Size(52, 21);
+            lblKullaniciAdi.TabIndex = 13;
+            lblKullaniciAdi.Text = "label7";
+            // 
+            // chbBilgilerimiGuncelle
+            // 
+            chbBilgilerimiGuncelle.AutoSize = true;
+            chbBilgilerimiGuncelle.Location = new Point(74, 414);
+            chbBilgilerimiGuncelle.Name = "chbBilgilerimiGuncelle";
+            chbBilgilerimiGuncelle.Size = new Size(163, 25);
+            chbBilgilerimiGuncelle.TabIndex = 14;
+            chbBilgilerimiGuncelle.Text = "Bilgilerimi Güncelle";
+            chbBilgilerimiGuncelle.UseVisualStyleBackColor = true;
+            // 
+            // FRMKullaniciBilgileri
+            // 
+            AutoScaleDimensions = new SizeF(9F, 21F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1029, 630);
+            Controls.Add(chbBilgilerimiGuncelle);
+            Controls.Add(lblKullaniciAdi);
+            Controls.Add(btnGuncelle);
+            Controls.Add(txtAd);
+            Controls.Add(txtSifre);
+            Controls.Add(txtYas);
+            Controls.Add(txtSoyad);
+            Controls.Add(lblSifre);
+            Controls.Add(lblYas);
+            Controls.Add(lblSoyad);
+            Controls.Add(label2);
+            Controls.Add(lbl);
+            Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 162);
+            Margin = new Padding(4, 4, 4, 4);
+            Name = "FRMKullaniciBilgileri";
+            Text = "FRMKullaniciBilgileri";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label lbl;
+        private Label label2;
+        private Label lblSoyad;
+        private Label lblYas;
+        private Label lblSifre;
+        private Label label6;
+        private TextBox txtSoyad;
+        private TextBox txtYas;
+        private TextBox txtSifre;
+        private TextBox textBox4;
+        private TextBox txtAd;
+        private Button btnGuncelle;
+        private Label lblKullaniciAdi;
+        private CheckBox chbBilgilerimiGuncelle;
+    }
+}
