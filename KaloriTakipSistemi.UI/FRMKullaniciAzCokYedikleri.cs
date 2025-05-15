af78a7beb434923994e84dc43e75017c54c91ffc
berkay <dastanberkay@gmail.com>
2025-05-15 17:33:08 +0300

    en az ve en cok tercih edilen yemekleri listele

--- KaloriTakipSistemi.UI/FRMKullaniciAzCokYedikleri.cs
+++ KaloriTakipSistemi.UI/FRMKullaniciAzCokYedikleri.cs
@@ -12,9 +12,49 @@ namespace KaloriTakipSistemi.UI.Models
 {
     public partial class FRMKullaniciAzCokYedikleri : Form
     {
+        private readonly Context.MyDbContext _context;
         public FRMKullaniciAzCokYedikleri()
         {
             InitializeComponent();
+            _context = new Context.MyDbContext();
+        }
+
+        private void FRMKullaniciAzCokYedikleri_Load(object sender, EventArgs e)
+        {
+           // en cok yedigi yemek
+           this.DataContext = _context;
+            var enCokYedigiYemek = _context.KullaniciYemekler
+                .GroupBy(k => k.YemekId)
+                .Select(g => new
+                {
+                    YemekId = g.Key,
+                    YemekAdet = g.Count()
+                })
+                .OrderByDescending(g => g.YemekAdet)
+                .FirstOrDefault();
+            if (enCokYedigiYemek != null)
+            {
+                var yemek = _context.Yemekler.Find(enCokYedigiYemek.YemekId);
+                lblEnCokYedigimYemek.Text = yemek?.Ad;
+            }
+            // en az yedigi yemek
+            var enAzYedigiYemek = _context.KullaniciYemekler
+                .GroupBy(k => k.YemekId)
+                .Select(g => new
+                {
+                    YemekId = g.Key,
+                    YemekAdet = g.Count()
+                })
+                .OrderBy(g => g.YemekAdet)
+                .FirstOrDefault();
+            if (enAzYedigiYemek != null)
+            {
+                var yemek = _context.Yemekler.Find(enAzYedigiYemek.YemekId);
+                lblEnAzYedigimYemek.Text = yemek?.Ad;
+            }
+
+
         }
     }
 }
+
